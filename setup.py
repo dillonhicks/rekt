5eae77c2e62dbeffb81ff753880a1369bc01c583
Dillon Hicks <chronodynamic@gmail.com>
2015-07-19 02:02:31 -0700

    comments

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
+"""
+Rekt
+====
+
+rekt is a wrapper around the requests library that makes generic rest
+operations less painful. I was frustrated with the implementation of
+many service specific rest wrappers especially for google apis, this
+library is meant to be generic and dynamic enough by templating the common
+client code for most rest services.
+"""
 import re
 import ast
 from setuptools import setup
