b19c91a55c2f6eb139a66b0868bfec6431b3335e
Dillon Hicks <chronodynamic@gmail.com>
2015-07-18 19:11:20 -0700

    preliminary setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+import re
+import ast
+from setuptools import setup
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('rekt/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+setup(
+    name='Rekt',
+    version=version,
+    url='http://github.com/vengefuldrx/rekt/',
+    license='Apache License Version 2',
+    author='Dillon Hicks',
+    author_email='chronodynamic@gmail.com',
+    description='A requests wrapper library for dynamically generating rest clients'
+    long_description=__doc__,
+    packages=['rekt'],
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'requests',
+        'PyYaml',
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    entry_points='''
+        [console_scripts]
+        rekt=rekt:main
+    '''
+)
